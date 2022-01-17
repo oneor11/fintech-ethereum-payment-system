05a5125e1256f9d53c558e1eb7fcf8eb009467a3
Jacob Rougeau <oneor11@hotmail.com>
2022-01-17 17:53:36 -0600

    Edit scaffolded code to make it more pythonic
    Adjusted comments and line spacing in crypto_wallet.py.

--- crypto_wallet.py
+++ crypto_wallet.py
@@ -1,10 +1,3 @@
-# Cryptocurrency Wallet
-################################################################################
-
-# This file contains the Ethereum transaction functions that you have created throughout this module’s lessons. By using import statements, you will integrate this `crypto_wallet.py` Python script into the Fintech Finder interface program that is found in the `fintech_finder.py` file.
-
-################################################################################
-# Imports
 import os
 import requests
 from dotenv import load_dotenv
@@ -14,11 +7,12 @@ from web3 import Account
 from web3 import middleware
 from web3.gas_strategies.time_based import medium_gas_price_strategy
 
-################################################################################
-# Wallet functionality
-
 def generate_account():
-    """Create a digital wallet and Ethereum account from a mnemonic seed phrase."""
+    """Create a digital wallet and Ethereum account.
+    
+    Uses mnemonic seed phrase stored in local .env file.
+    """
+
     # Fetch mnemonic from environment variable.
     mnemonic = os.getenv("MNEMONIC")
 
@@ -33,8 +27,10 @@ def generate_account():
 
     return account
 
+
 def get_balance(w3, address):
     """Using an Ethereum account address access the balance of Ether"""
+
     # Get balance of address in Wei
     wei_balance = w3.eth.get_balance(address)
 
@@ -47,6 +43,7 @@ def get_balance(w3, address):
 
 def send_transaction(w3, account, to, wage):
     """Send an authorized transaction to the Ganache blockchain."""
+
     # Set gas price strategy
     w3.eth.setGasPriceStrategy(medium_gas_price_strategy)
 
