f4dd744b32d37d2dd3a072deccaf9968d8ef9354
Jacob Rougeau <62037861+oneor11@users.noreply.github.com>
2022-02-09 12:45:42 -0600

    Update fintech_finder.py

--- fintech_finder.py
+++ fintech_finder.py
@@ -1,27 +1,3 @@
-# Cryptocurrency Wallet
-
-################################################################################
-# For this Challenge, you will assume the perspective of a Fintech Finder
-# customer in order to do the following:
-
-# * Generate a new Ethereum account instance by using your mnemonic seed phrase
-
-# * Fetch and display the account balance associated with your Ethereum account
-# address.
-
-# * Calculate the total value of an Ethereum transaction, including the gas
-# estimate, that pays a Fintech Finder candidate for their work.
-
-# * Digitally sign a transaction that pays a Fintech Finder candidate, and send
-# this transaction to the Ganache blockchain.
-
-# * Review the transaction hash code associated with the validated blockchain transaction.
-
-# Once you receive the transaction’s hash code, you will navigate to the Transactions
-# section of Ganache to review the blockchain transaction details. 
-
-
-# Imports
 import streamlit as st
 from dataclasses import dataclass
 from typing import Any, List
